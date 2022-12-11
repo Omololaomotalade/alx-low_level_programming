0ae8a604f8d9775e4945c79a86a4a1e1a0731012
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-11 10:30:54 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int n;
 
 srand(time(0));
 
-n=rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 printf("is positive\n", n);
@@ -19,5 +19,5 @@ else if (n == 0)
 printf("is zero\n", n);
 else
 printf("is negative\n", n);
-return(0);
+return (0);
 }

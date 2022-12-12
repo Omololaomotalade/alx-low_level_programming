8e9b253b437f4d5694622583eac9ba817d24d79c
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-12 08:28:30 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphebet;
+	
+	if (alphebet != 'q' && alphebet != 'e')
+
+		for (alphebet = 'a'; alphebet <= 'z', alphebet++)
+			putchar(alphebet);
+	putchar('\n');
+
+	return (0);
+}

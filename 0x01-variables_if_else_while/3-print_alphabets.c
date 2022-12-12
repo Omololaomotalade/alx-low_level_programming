1dbb94b76291f205a95b30a8fcbf814b2756eba6
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-12 07:32:41 -0800

    3-prin_alphebets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,6 +10,7 @@ int main(void)
 	char alphebet;
 
 	for (alphebet = 'a'; alphebet <= 'z'; alphebet++)
+		 putchar(alphebet);
 	for (alphebet = 'A'; alphebet <= 'Z'; alphebet++)
 		putchar(alphebet);
 putchar('\n');

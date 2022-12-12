eb02291e0699605fc63d39ac31e651c830a8399b
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-12 06:01:03 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,14 +19,14 @@ n = rand() - RAND_MAX / 2;
 
 last_num = n % 10;
 
-printf("Last digit of %d is %d", n, last_num);
+
 
 if (last_num > 5)
-printf("and is greater than \n", n);
+printf("Last digit of %d is %d and is greater than \n", n, last_num);
 else if (last_num == 0)
-printf("and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n,last_num);
 else
-printf("and is less than 6 and not 0", n);
+printf("Last digit of %d is %d and is less than 6 and not 0", n,last_num);
 
 return (0);
 

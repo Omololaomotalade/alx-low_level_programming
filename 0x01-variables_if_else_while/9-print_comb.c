05cde0c278866a24f9fb47bbd7dbdfc48803e603
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-12 15:42:48 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	int number;
-	
-	for (number = 0; number <=9; number++)
+	int number;	
+	for (number = 0; number <= 9; number++)
 	
 	{
 		putchar(number + '0')
 
-		if (number !=10)
+		if (number != 9)
 		{
 			putchar(',');
 			putchar(' ');

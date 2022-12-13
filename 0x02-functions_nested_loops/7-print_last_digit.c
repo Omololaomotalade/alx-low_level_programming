1573d5231db47d97c76e99d433f352bb17267fc9
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-13 13:07:34 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit(int) - checks the last digit of a number
+ * @r: the character
+ * Return: last digit of r , if not 0
+ */
+int print_last_digit(int n)
+{
+	n = n%10
+
+		if (n > 0)
+		{
+			return(n);
+		}
+		else 
+		{
+			return (0);
+		}
+}
+

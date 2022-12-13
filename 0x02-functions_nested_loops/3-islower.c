b3d872cffd65f94db489c51bc0621daa193c4bdf
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-13 09:07:40 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int_islower - check the code for ALX School students.
+ *
+ * Return: 1 if c is lowercase otherwise 0.
+ */
+int_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+

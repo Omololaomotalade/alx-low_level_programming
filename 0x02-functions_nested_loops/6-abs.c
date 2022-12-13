89cfc601b8333516243979dff1e5e3595c685edf
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-13 12:42:16 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * a function that computes the absolute value of an integer
+ * prototype int _abs(int)
+ * r is the variable
+ */
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else 
+		return (-r)
+}

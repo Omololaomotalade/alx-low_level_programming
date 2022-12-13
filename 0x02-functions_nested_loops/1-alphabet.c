6b70b42f034658a4dd78af9a6ccb00b99f32ec71
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-13 07:19:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+_putchar('\n');
+
+return (0);
+
+}

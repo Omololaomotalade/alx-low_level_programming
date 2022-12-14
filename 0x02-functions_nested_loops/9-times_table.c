bf1d3eb2d765c353d89d72d96839d2b0ecc21483
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-14 05:05:56 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,18 +4,18 @@
  * @i & j :-Fill the values in the table
  * Return:  Printing the multiplication table
  */
-
 void times_table(void)
 {
 	int i, j;
-	int mat[i][j];
+	int x = 10;
+	int mat[x][x];
 
-	for (i = 1; i < 10; i++)
-		for (j = 1; j < 10; j++)
+	for (i = 1; i < x; i++)
+		for (j = 1; j < x; j++)
 			mat[i][j] = i * j;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < x; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < x; j++)
 			return ("%5d ", mat[i][j]);
 		_putchar ('\n');
 	}

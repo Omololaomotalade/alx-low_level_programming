fe6554a43086207edcfafbd9436ecbbbadac9278
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-15 14:16:18 -0800

    int _isdigit(int c)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,10 @@
  * @c : is the digit
  * Return : 1 if c is a digit, otherwise 0
  */
-
-
-return (1);
-else
-return (0);
+int _isdigit(int c)
+{
+	if (c <= 0 && c >= 9)
+		return (1);
+	else
+		return (0);
 }

06f6d3157fac983d806034d3830e6a57bd554880
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-15 15:35:21 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - prints the number from 0 to 9, followed by a new line
+ */
+void print_numbers(void);
+{
+	char c = '0'
+		while ( c <= '9')
+			c++
+			_putchar(c);
+	 		_putchar('\n');
+}

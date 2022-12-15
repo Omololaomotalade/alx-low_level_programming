9cbbb72c13ef77e023a799e8fb24658702d802db
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-15 14:14:06 -0800

    main.h

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,8 @@
  * @c : is the digit
  * Return : 1 if c is a digit, otherwise 0
  */
-int _isdigit(int c)
-{
-if (c >= 0 && c <= 9)
+
+
 return (1);
 else
 return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,6 +15,4 @@ void print_number(int n);
 
 
 
-
-
 #endif

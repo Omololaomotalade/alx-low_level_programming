93fb9d4478faaa26a811ec586cab22a2fb9f2590
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-15 16:15:10 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int c = 48;
+int c = 48;
 
-	while (c < 58)
-	{
-		if (c != 50 && c != 52)
-		{
-			_putchar(c);
-			c++;
-		}
-	}
-	_putchar('\n');
+while (c < 58)
+{
+if (c != 50 && c != 52)
+{
+_putchar(c);
+c++;
+}
+}
+_putchar('\n');
 }

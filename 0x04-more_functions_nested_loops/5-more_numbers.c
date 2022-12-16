3a181797f06b37bf86033d2d0c213728b1bf82fe
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-15 16:54:37 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,11 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar(j + '0');
+		{
+			if (i >= 10)
+				_putchar('1);
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n')
 }

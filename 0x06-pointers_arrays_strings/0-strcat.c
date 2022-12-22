9419e4f16f17d4502ff6b90d9e2e3be0ec7c6d17
Omololaomotalade <omololaomotalade@yahoo.com>
2022-12-22 02:25:54 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/** _strcat - a function that concatenates two strings
+ * @src: a string
+ * @dest: a string
+ * Return dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len1, len2;
+	len1 = strlen(dest);
+	len2 = strlen(src);
+	
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1+i] = src[i]
+	}
+
+	return (dest);

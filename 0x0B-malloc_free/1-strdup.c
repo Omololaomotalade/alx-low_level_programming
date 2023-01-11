8c6d86efcf50b904bbc72b712f8a45245f17f38f
Omololaomotalade <omololaomotalade@yahoo.com>
2023-01-11 08:18:07 -0800

    1-strdu

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,4 +13,27 @@ char *_strdup(char *str)
 	/* check is str is null */
 	if (str == NULL)
 	{
+		return (NULL);
+	}
 
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	/*check if malloc was successful*/
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+}

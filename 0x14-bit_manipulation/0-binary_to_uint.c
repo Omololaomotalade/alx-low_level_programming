4863dfdff24742e3af18bd3a11a632f00e000966
Omololaomotalade <omololaomotalade@yahoo.com>
2023-02-03 14:20:37 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int dec_val = 0;
+	int j;
 
 	if (!b)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (j = 0; b[j]; j++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		dec_val = 2 * dec_val + (b[j] - '0');
 	}
-
-	return (ui);
+	return (dec_val);
 }
